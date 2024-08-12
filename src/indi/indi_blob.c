cb66e47fcca9cbba025b7edac873675cb518852f
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-08-12 15:29:06 +0200

    update

---
+++ src/indi/indi_blob.c
@@ -0,0 +1,104 @@
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#include "../nyx_node_internal.h"
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+static void debug_callback(nyx_object_t *object)
+{
+    nyx_dict_t *dict = nyx_blob_set_vector_new((nyx_dict_t *) object);
+
+    str_t json = nyx_dict_to_string(dict);
+    printf("** \033[91mNOT REGISTERED\033[0m **\n%s\n", json);
+    nyx_memory_free(json);
+
+    nyx_dict_free(dict);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* DEF                                                                                                                */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+nyx_dict_t *nyx_blob_def_new(STR_t name, __NULLABLE__ STR_t label, STR_t value)
+{
+    if(label == NULL || label[0] == '\0')
+    {
+        label = name;
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    nyx_dict_t *result = nyx_dict_new();
+
+    nyx_dict_set(result, "<>", nyx_string_from("defSwitch"));
+
+    nyx_dict_set(result, "@name", nyx_string_from(name));
+    nyx_dict_set(result, "@label", nyx_string_from(label));
+
+    nyx_dict_set(result, "$", nyx_string_from(value));
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    return result;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* DEF VECTOR                                                                                                         */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+nyx_dict_t *nyx_blob_def_vector_new(
+    STR_t device,
+    STR_t name,
+    nyx_state_t state,
+    nyx_perm_t perm,
+    nyx_dict_t *defs[],
+    nyx_opts_t *opts
+) {
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    nyx_dict_t *result = nyx_dict_new();
+
+    nyx_list_t *children = nyx_list_new();
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    nyx_dict_set(result, "<>", nyx_string_from("defBLOBVector"));
+
+    nyx_dict_set(result, "children", children);
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    nyx_dict_set(result, "@client", nyx_string_from("unknown"));
+    nyx_dict_set(result, "@device", nyx_string_from(device));
+    nyx_dict_set(result, "@name", nyx_string_from(name));
+
+    nyx_dict_set(result, "@state", nyx_string_from(nyx_state_to_str(state)));
+    nyx_dict_set(result, "@perm", nyx_string_from(nyx_perm_to_str(perm)));
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    internal_set_opts(result, opts);
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    for(; *defs != NULL; defs++) nyx_list_push(children, *defs);
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    result->base.out_callback = debug_callback;
+
+    return result;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* SET VECTOR                                                                                                         */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+nyx_dict_t *nyx_blob_set_vector_new(const nyx_dict_t *def_vector)
+{
+    return internal_xxx_set_vector_new(def_vector, "setBLOBVector", "oneBLOB");
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
---
+++ src/indi/indi_del_property.c
@@ -0,0 +1,40 @@
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+#include "../nyx_node_internal.h"
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+nyx_dict_t *nyx_del_property_new(STR_t device, __NULLABLE__ STR_t name, __NULLABLE__ STR_t message)
+{
+    nyx_dict_t *result = nyx_dict_new();
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    char timestamp[32];
+
+    internal_get_timestamp(timestamp, sizeof(timestamp));
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    nyx_dict_set(result, "<>", nyx_string_from("delProperty"));
+
+    nyx_dict_set(result, "@client", nyx_string_from("unknown"));
+    nyx_dict_set(result, "@device", nyx_string_from(device));
+    nyx_dict_set(result, "@timestamp", nyx_string_from(timestamp));
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    if(name != NULL) {
+        nyx_dict_set(result, "@name", nyx_string_from(name));
+    }
+
+    if(message != NULL) {
+        nyx_dict_set(result, "@message", nyx_string_from(message));
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    return result;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
