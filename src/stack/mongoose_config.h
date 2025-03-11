0ef1309e62756bcf71e510b07b36812908650d5a
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-03-11 12:25:44 +0100

    update

--- src/stack/mongoose_config.h
+++ src/stack/mongoose_config.h
@@ -1,16 +1,17 @@
 #pragma once
 
-#ifdef PICO_BOARD
+#if defined(PICO_BOARD)
 #  define MG_ARCH MG_ARCH_PICOSDK
 #endif
 
-#ifdef ARDUINO
-#  ifdef ESP8266
+#if defined(ARDUINO)
+#  if defined(ESP8266)
 #    define MG_ARCH MG_ARCH_ESP8266
-#  elif ESP32
+#  elif defined(ESP32)
 #    define MG_ARCH MG_ARCH_ESP32
 #  else
 #    define MG_ARCH MG_ARCH_CUSTOM
+#    define MG_ENABLE_DRIVER_W5500 1
 #    define MG_ENABLE_SOCKET 0
 #    define MG_ENABLE_TCPIP 1
 #    define mkdir(a, b) (-1)
