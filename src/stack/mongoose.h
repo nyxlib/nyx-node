5ed2d03efd8b05504dbe5e72f19ba98404544d4f
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-03-12 12:36:10 +0100

    update

--- src/stack/mongoose.h
+++ src/stack/mongoose.h
@@ -53,26 +53,27 @@ extern "C" {
 #endif  // !defined(MG_ARCH)
 
 #if !defined(MG_ARCH) || (MG_ARCH == MG_ARCH_CUSTOM)
-#   if defined(PICO_BOARD)
+# if defined(ARDUINO)
+#   if defined(ESP8266)
+#     define MG_ARCH MG_ARCH_ESP8266
+#   elif defined(ESP32)
+#     define MG_ARCH MG_ARCH_ESP32
+#   elif defined(PICO_BOARD)
 #     define MG_ARCH MG_ARCH_PICOSDK
 #     define MG_ENABLE_DRIVER_W5500 1
 #     define MG_ENABLE_SOCKET 0
 #     define MG_ENABLE_TCPIP 1
-#   endif
-
-#   if defined(ARDUINO)
-#     if defined(ESP8266)
-#       define MG_ARCH MG_ARCH_ESP8266
-#     elif defined(ESP32)
-#       define MG_ARCH MG_ARCH_ESP32
-#     else
-#       define MG_ARCH MG_ARCH_CUSTOM
-#       define MG_ENABLE_DRIVER_W5500 1
-#       define MG_ENABLE_SOCKET 0
-#       define MG_ENABLE_TCPIP 1
-#       define mkdir(a, b) (-1)
-#       define MG_IO_SIZE 128
-#     endif
+#     define mkdir(a, b) (-1)
+#     define MG_IO_SIZE 512
+#   else
+#     define MG_ARCH MG_ARCH_CUSTOM
+#     define MG_ENABLE_DRIVER_W5500 1
+#     define MG_ENABLE_SOCKET 0
+#     define MG_ENABLE_TCPIP 1
+#     define mkdir(a, b) (-1)
+#     define MG_IO_SIZE 128
+#   else
+#     error("Only ARDUINO platform is supported")
 #   endif
 #endif
 
