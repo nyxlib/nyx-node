3474de657007279f93eed5c2039ef3d7170a7b45
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-03-22 20:15:53 +0100

    update

--- src/log.c
+++ src/log.c
@@ -11,3 +11,24 @@ nyx_log_func_t nyx_log_func = NULL;
 buff_t nyx_log_args = NULL;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+STR_t nyx_log_level_to_str(nyx_log_level_t level)
+{
+    switch(level)
+    {
+        case NYX_LOGGER_NONE:
+            return "NONE";
+        case NYX_LOGGER_ERROR:
+            return "ERROR";
+        case NYX_LOGGER_INFO:
+            return "INFO";
+        case NYX_LOGGER_DEBUG:
+            return "DEBUG";
+        case NYX_LOGGER_VERBOSE:
+            return "VERBOSE";
+    }
+
+    return "???";
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
--- src/stack/mongoose_stack.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jodier on 22/03/25.
-//
-
-#ifndef NYX_NODE_MONGOOSE_STACK_H
-#define NYX_NODE_MONGOOSE_STACK_H
-
-#endif //NYX_NODE_MONGOOSE_STACK_H
