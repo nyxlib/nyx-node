fd0647e0a17336a468876f100cb381965a07290b
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-06-26 10:58:24 +0200

    update

--- nyx_node/indi/__init__.py
+++ nyx_node/indi/__init__.py
@@ -5,7 +5,14 @@
 # SPDX-License-Identifier: GPL-3.0+
 ########################################################################################################################
 
+from .indi_blob import *
 from .indi_text import *
+from .indi_light import *
+from .indi_number import *
+from .indi_stream import *
+from .indi_switch import *
+from .indi_message import *
+from .indi_del_property import *
 
 ########################################################################################################################
 
